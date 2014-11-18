71b96ceea1d6d7dbf593850e40caefd99a65b293
renatacorrea <renata.correa.br@gmail.com>
2014-11-18 21:54:25 -0200

    Aula06

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+      
+      [Key]
+        public int ProdutotId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
